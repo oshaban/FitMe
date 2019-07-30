994f0e3a89cde5b34cbe3856a37d895b53d64733
LAPTOP-DHPAEE97\Omar <omar.shaban@rutgers.edu>
2019-07-29 21:19:44 -0400

    Added UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Importing components to register in router:
 import { HomeComponent } from './pages/home/home.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 // Registering components in router:
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'stats',
+    component: StatsComponent
+  },
+  { path: '**', component: HomeComponent } /* Wild-card route */
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +10,27 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
+/* Angular material: https://material.angular.io/*/
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+import { StatsComponent } from './pages/stats/stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    StatsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     Title,

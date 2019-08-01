0c1d39bb0c20b6c6550324e6fc77bdddc897da07
LAPTOP-DHPAEE97\Omar <omar.shaban@rutgers.edu>
2019-07-31 22:08:11 -0400

    Login screen done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userDetailsForm = this.fb.group({
+    username : ['', [
+        Validators.required,
+      ]
+    ],
+    password : ['', [
+      Validators.required,
+      ]
+    ],
+  });
+
+  // Error messages for user validation
+    // Can access in profile.component.html by: *ngFor="let validation of userValidationMessage.<username>
+  userValidationMessage = {
+    username: [
+      { type: 'required', message: 'Username is required' },
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' },
+    ],
+
+
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.userDetailsForm.valid) {
+      console.log(this.userDetailsForm.value);
+    }
   }
 
 }
